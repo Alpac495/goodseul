56cf7ee1f149e2e934d6feb2b2e3e05b0cf7124f
helloa1109 <helloa1109@naver.com>
2023-09-15 22:23:25 +0900

    서브헤더 및 라우터 세팅 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import Header from './component/Header/Header';
 import Main from './component/Main/Main';
 import Login from './component/Login/Login';
 import Join from './component/Join/Join';
-
+import SubHeader from './component/Header/SubHeader';
+import Test1 from './component/Main/Test1';
 
 function App() {
+  
   return (
     <BrowserRouter>
-    <Header/>
+    {window.location.pathname === '/' ? <Header /> : <SubHeader/>}
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/login' element={<Login/> }/>
         <Route path='/join' element={<Join/> }/>
+        <Route path='/test' element={<Test1/>}/>
       </Routes>
     </BrowserRouter>
   );
