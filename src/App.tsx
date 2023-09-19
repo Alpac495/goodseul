eecb48e76be36e1be0124e350b445a0e19fe4796
Deko252 <dbcksals555@naver.com>
2023-09-19 10:57:29 +0900

    test:로그인 테스트코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './component/Header/Header';
 import Main from './component/Main/Main';
+import Login from './component/Login/Login';
+import Join from './component/Join/Join';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <Header/>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Login/> }/>
+        <Route path='/join' element={<Join/> }/>
       </Routes>
     </BrowserRouter>
   );
--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import '../../style/Login/Login.scss';
 
 const Login = () => {
   return (
     <div>
-      
+        <div>
+            <div>구슬에 로그인 하세요.</div>
+            <form>
+                <div> 
+                    <div><input type="email" /></div>
+                    <div><input type="password" /></div>
+                </div>
+                <div>
+                    <a href="#">아이디/비밀번호 찾기</a>
+                    &nbsp;|&nbsp;
+                    <a href="./join">회원가입</a>
+                </div>
+                <div>
+                    <button>이메일 로그인</button>
+                </div>
+                <div>
+                    <div>
+                        <button>네이버 로그인</button>
+                    </div>
+                    <div>
+                        <button>카카오 로그인</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
