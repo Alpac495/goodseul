7a1fbfcc20b26b3ff927ee25a663eaddc717ad43
Deko252 <dbcksals555@naver.com>
2023-09-19 11:41:19 +0900

    test:로그인/회원가입 테스트코드

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,12 @@ function App() {
     {window.location.pathname === '/' ? <Header /> : <SubHeader/>}
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Login/> }/>
-        <Route path='/join' element={<Join/> }/>
         <Route path='/test' element={<Test1/>}/>
         <Route path="/*" element={
                         <h1 style={{ width: "100%", textAlign: "center", marginTop: "10%", position: "absolute" }}>페이지가 없습니다</h1>
                     } />
+        <Route path='/login' element={<Login/> }/>
+        <Route path='/join' element={<Join/> }/>
       </Routes>
     </BrowserRouter>
   );
